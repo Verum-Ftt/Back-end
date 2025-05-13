7e1716bd6be24f69a314389077341f54e356d2d1
Bruce Wayne <oakhenry2@gmail.com>
2025-05-13 11:12:41 -0300

    Feat: new http controllers

---
+++ src/infrastructure/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { Env } from "@/env"
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
+import { ConfigService } from "@nestjs/config";
+import { PassportModule } from "@nestjs/passport";
+import { AuthenticateController } from "../../interfaces/auth/authenticate.controller";
+import { PrismaService } from "@/infrastructure/prisma/prisma.service";
+
+
+@Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            global: true,
+            useFactory(config: ConfigService<Env, true>){
+                    const privateKey = config.get('JWT_PRIVATE_KEY', {infer: true})
+                    const publicKey = config.get('JWT_PUBLIC_KEY', {infer: true})
+
+                return{
+                    signOptions: { algorithm: 'RS256' },
+                    privateKey: Buffer.from(privateKey,'base64'),
+                    publicKey: Buffer.from(publicKey,'base64'),
+                }
+            }
+        }),
+    ],
+
+    controllers: [ AuthenticateController ],
+
+    providers: [JwtStrategy, PrismaService]
+})
+
+
+export class AuthModule{}
---
+++ src/interfaces/http/event/fecth-event.controller.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { AuthGuard } from "@nestjs/passport";
+import { PrismaService } from "@/infrastructure/prisma/prisma.service";
+import { ZodValidationPipe } from "@/application/pipes/zod-valitation-pipe";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/events')
+@UseGuards(AuthGuard('jwt'))
+export class FecthEventController{
+    constructor( private prisma: PrismaService ) {}
+
+    @Get('/get')
+    async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+        const perPage = 20
+        const events = await this.prisma.events.findMany({
+            take: perPage,
+            skip: (page - 1) * perPage,
+            orderBy: {
+                date_created: "desc"
+            }
+        })
+
+        return { events }
+    }
+}
